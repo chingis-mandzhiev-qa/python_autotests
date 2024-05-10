c2c613033c66464fddd05c3b255b7c465a1ecc57
chingis-mandzhiev-qa <166165772+chingis-mandzhiev-qa@users.noreply.github.com>
2024-05-10 17:08:08 +0300

    Add files via upload

---
+++ PythonProjects/main.py
@@ -0,0 +1,31 @@
+import requests
+
+URL = 'https://api.pokemonbattle.me/v2'
+TOKEN = 'c01556d828df71a34e6ac0b683fadcb0'
+HEADER = {'Content-Type': 'application/json', 'trainer_token': TOKEN}
+body_create = {
+    "name": "generate",
+    "photo": "generate"
+}
+
+body_update = {
+    "pokemon_id": "26543",
+    "name": "The Wasp",
+    "photo": "https://dolnikov.ru/pokemons/albums/014.png"
+}
+
+body_catch = {
+    "pokemon_id": "26618"
+}
+
+response_create = requests.post(url = f'{URL}/pokemons', headers = HEADER, json = body_create)
+print(response_create.text)
+
+'''response_update = requests.put(url = f'{URL}/pokemons', headers = HEADER, json = body_update)
+print(response_update.text)'''
+
+'''response_catch = requests.post(url = f'{URL}/trainers/add_pokeball', headers = HEADER, json = body_catch)
+print(response_catch.text)'''
+
+message = response_create.json()['message']
+print(message) 
---
+++ PythonProjects/tests/test_pokemons.py
@@ -0,0 +1,22 @@
+import requests
+import pytest
+
+URL = 'https://api.pokemonbattle.me/v2'
+TOKEN = 'c01556d828df71a34e6ac0b683fadcb0'
+HEADER = {'Content-Type': 'application/json', 'trainer_token': TOKEN}
+TRAINER_ID = '2179'
+
+def test_status_code():
+    response = requests.get(url = f'{URL}/trainers')
+    assert response.status_code == 200
+
+def test_part_of_response():
+    response_get = requests.get(url = f'{URL}/trainers', params = {'trainer_id' : TRAINER_ID})
+    assert response_get.json()['data'][0]['trainer_name'] == 'Jonas'
+
+
+@pytest.mark.parametrize('key,value', [('trainer_name', 'Jonas'),('id', TRAINER_ID),('level', '5')])
+
+def test_parametrize(key, value):
+    response_parametrize = requests.get(url = f'{URL}/trainers', params = {'trainer_id' : TRAINER_ID})
+    assert response_parametrize.json()['data'][0][key] == value 
